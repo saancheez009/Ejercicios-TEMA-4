7a28b03d4aae17030a034d9e1c47a4ec8dbe9d5a
bsanchez <bsanchez@106-27.info2>
2022-12-05 09:16:36 +0100

    commit

--- EjerciciosTema4/src/Ejercicio5/FuncionesRecursivas.java
+++ EjerciciosTema4/src/Ejercicio5/FuncionesRecursivas.java
@@ -1,10 +1,52 @@
 package Ejercicio5;
 
+
 public class FuncionesRecursivas {
+	
+	static int factorial=1;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int serieFibonacci(int numero) {
+		
+		int resultado;
+		
+		if(numero<=1) {
+			resultado=1;
+			
+		} else {
+			resultado= serieFibonacci(numero-1) + serieFibonacci (numero-2);
+		}
+	
+	return resultado;
+	
+	}
+	
+	static int sumatorio(int numero) {
+		
+		
+		for (int i=1;i<=numero;i++) {
+			  factorial = factorial + i;
+		}
+		/*factorial=numero*factorial*(numero-1);*/
+		
+		return factorial-1;
 	}
 
+	static double potencia (double a , int n) {
+		
+		double resultado = 0;
+		
+		if(n==0) {
+			resultado=1;
+		}else {
+			for (int i=1;i<=n;i++) {
+				  resultado = n * i;
+		}
+		
+	}
+		return resultado;
+
+	
+	
+	}
+	
 }
--- EjerciciosTema4/src/Ejercicio5/Main.java
+++ EjerciciosTema4/src/Ejercicio5/Main.java
@@ -1,10 +1,30 @@
 package Ejercicio5;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	int numero;
+	double a;
+	int n;
+	
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.println("Por favor introduzca un número ");
+	numero=sc.nextInt();
 
+	System.out.println("Por favor introduzca un número base (a) y su exponente (n)");
+	a=sc.nextInt();
+	n=sc.nextInt();
+	
+	System.out.println("La suma de los números consecutivos hasta el número introducido es : "+FuncionesRecursivas.sumatorio(numero));
+	
+	System.out.println("El resultado de a^n, es: "+FuncionesRecursivas.potencia(a,n));
+	
+	System.out.println("El resultado del número correspondiente de la serie de Fibonacci es: "+ FuncionesRecursivas.serieFibonacci(numero));
 	}
 
 }
+
